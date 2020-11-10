e91805684b6c2af30b6d3777828e5c90bdda59d5
SonuKumar81800 <sonu.patna0808@gmail.com>
2020-11-10 17:21:26 +0530

    Log

--- script.js
+++ script.js
@@ -73,116 +73,121 @@ $(document).ready(function () {
     },
   });
 
+  console.log("Particle js Script Started")
+
   particlesJS("particles-js", {
-      particles: {
-        number: {
-          value: 160,
-          density: {
-            enable: false,
-            value_area: 1000
+    particles: {
+      number: {
+        value: 160,
+        density: {
+          enable: false,
+          value_area: 1000
+        }
+      },
+      color: {
+        value: "#ffffff"
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000"
+        },
+        polygon: {
+          nb_sides: 5
+        },
+        image: {
+          // src: "img/github.svg",
+          width: 100,
+          height: 100
+        },
+      },
+      opacity: {
+        value: 0.4,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 1,
+          opacity_min: 0.2,
+          sync: false
+        },
+      },
+      size: {
+        value: 5,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 40,
+          size_min: 0.2,
+          sync: true
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 90,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 3,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: true,
+        attract: {
+          enable: true,
+          rotateX: 600,
+          rotateY: 1200
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "grab"
+        },
+        onclick: {
+          enable: true,
+          mode: "push"
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 150,
+          line_linked: {
+            opacity: 1
           }
         },
-        color: {
-          value: "#ffffff"
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3
         },
-        shape: {
-          type: "circle",
-          stroke: {
-            width: 0,
-            color: "#000000"
-          },
-          polygon: {
-            nb_sides: 5
-          },
-          image: {
-            // src: "img/github.svg",
-            width: 100,
-            height: 100
-          },
+        repulse: {
+          distance: 200,
+          duration: 0.4
         },
-        opacity: {
-          value: 0.4,
-          random: true,
-          anim: {
-            enable: true,
-            speed: 1,
-            opacity_min: 0.2,
-            sync: false
-          },
+        push: {
+          particles_nb: 5
         },
-        size: {
-          value: 5,
-          random: true,
-          anim: {
-            enable: true,
-            speed: 40,
-            size_min: 0.2,
-            sync: true
-          },
-        },
-        line_linked: {
-          enable: true,
-          distance: 90,
-          color: "#ffffff",
-          opacity: 0.4,
-          width: 1,
-        },
-        move: {
-          enable: true,
-          speed: 3,
-          direction: "none",
-          random: true,
-          straight: false,
-          out_mode: "out",
-          bounce: true,
-          attract: {
-            enable: true,
-            rotateX: 600,
-            rotateY: 1200
-          },
+        remove: {
+          particles_nb: 2
         },
       },
-      interactivity: {
-        detect_on: "canvas",
-        events: {
-          onhover: {
-            enable: true,
-            mode: "grab"
-          },
-          onclick: {
-            enable: true,
-            mode: "push"
-          },
-          resize: true,
-        },
-        modes: {
-          grab: {
-            distance: 150,
-            line_linked: {
-              opacity: 1
-            }
-          },
-          bubble: {
-            distance: 400,
-            size: 40,
-            duration: 2,
-            opacity: 8,
-            speed: 3
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4
-          },
-          push: {
-            particles_nb: 5
-          },
-          remove: {
-            particles_nb: 2
-          },
-        },
-      },
-      retina_detect: true,
-    });
+    },
+    retina_detect: true,
+  });
+
+  console.log("Particle js Script End")
+
 });
 
 //Particle js script
