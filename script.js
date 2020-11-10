f1e3df4a2949f4cf613050a2954a86e93744702f
SonuKumar81800 <sonu.patna0808@gmail.com>
2020-11-10 13:15:01 +0530

    Improve Performance

--- script.js
+++ script.js
@@ -1,74 +1,77 @@
 $(document).ready(function () {
-	$(window).scroll(function () {
-		// sticky navbar on scroll script
-		if (this.scrollY > 20) {
-			$(".navbar").addClass("sticky");
-		} else {
-			$(".navbar").removeClass("sticky");
-		}
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
 
-		// scroll-up button show/hide script
-		if (this.scrollY > 500) {
-			$(".scroll-up-btn").addClass("show");
-		} else {
-			$(".scroll-up-btn").removeClass("show");
-		}
-	});
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+      $(".icon-bar").addClass("showWa");
 
-	// slide-up script
-	$(".scroll-up-btn").click(function () {
-		$("html").animate({ scrollTop: 0 });
-		// removing smooth scroll on slide-up button click
-		$("html").css("scrollBehavior", "auto");
-	});
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+      $(".icon-bar").removeClass("showWa");
+    }
+  });
 
-	$(".navbar .menu li a").click(function () {
-		// applying again smooth scroll on menu items click
-		$("html").css("scrollBehavior", "smooth");
-	});
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
 
-	// toggle menu/navbar script
-	$(".menu-btn").click(function () {
-		$(".navbar .menu").toggleClass("active");
-		$(".menu-btn i").toggleClass("active");
-	});
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
 
-	// typing text animation script
-	var typed = new Typed(".typing", {
-		strings: ["Developer", "Gamer", "Designer", "Freelancer"],
-		typeSpeed: 100,
-		backSpeed: 60,
-		loop: true,
-	});
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-	var typed = new Typed(".typing-2", {
-		strings: ["Developer", "Gamer", "Designer", "Freelancer"],
-		typeSpeed: 100,
-		backSpeed: 60,
-		loop: true,
-	});
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Developer", "Gamer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-	// owl carousel script
-	$(".carousel").owlCarousel({
-		margin: 20,
-		loop: true,
-		autoplayTimeOut: 1000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-				nav: false,
-			},
-			600: {
-				items: 2,
-				nav: false,
-			},
-			1000: {
-				items: 3,
-				nav: false,
-			},
-		},
-	});
+  var typed = new Typed(".typing-2", {
+    strings: ["Developer", "Gamer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 1000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 });
 
 //Particle js script
